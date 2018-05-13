b56295ea9f4460568c8a7588fc521c8f16b308d9
Van Dat <fanh.troll@gmail.com>
2018-05-13 15:52:42 +0700

    ex

--- Java_Course_TH06/src/vandat/mai/hueic/edu/vn/IHuman.java
+++
@@ -1,7 +0,0 @@
-package vandat.mai.hueic.edu.vn;
-
-public interface IHuman {
-    public Object Timkiem (int from, int to);
-    public Object TimKiem (boolean gt);
-    public Object TimKiem (String ten);
-}
